76bb96829ef9bea944e74167b4e602468725558d
Mainafrancis <francis.maina@student.moringaschool.com>
2022-05-01 14:49:31 +0300

    function

--- manage.py
+++ manage.py
@@ -16,3 +16,10 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, Pitch = Pitch, User = User, Comment = Comment)
+
+if __name__=='__main__':
+    manager.run()        
