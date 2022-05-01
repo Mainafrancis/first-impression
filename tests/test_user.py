e6eaa21cadb7fd8b616af0a175fb1a6e00e46f33
Mainafrancis <francis.maina@student.moringaschool.com>
2022-05-01 15:17:56 +0300

    password verification

--- tests/test_user.py
+++ tests/test_user.py
@@ -7,4 +7,10 @@ class UserTest(unittest.TestCase):
 
     def test_password_setter(self):
         self.assertTrue(self.new_user.pass_secure is not None)
-            
+
+    def test_no_access_password(self):
+        with self.asssertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('banana'))                
